8e4316b96f6791c030362b524eedd4d82b693ddd
Chheng Chanbo <chanbochheng4@gmail.com>
2026-06-02 01:21:14 +0700

    Fix Cloudinary cloud name

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -139,10 +139,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME", "dqreww6yt"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY", "898997961179595"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET", "VEgWXoTVsgZ9L3-sPmtHMIVJ1QM"),
 }
+
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
@@ -150,5 +151,4 @@ STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
-}
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+} 
