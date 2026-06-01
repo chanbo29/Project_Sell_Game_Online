f8465a91aa219c42e2c29a4d2432825b289fa8f4
Chheng Chanbo <chanbochheng4@gmail.com>
2026-06-02 01:41:08 +0700

    Fix Render database config

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -82,14 +82,16 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 import os
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if not os.environ.get("DATABASE_URL"):
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
