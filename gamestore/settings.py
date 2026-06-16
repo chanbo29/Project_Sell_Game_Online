ab1f64664f11600984a699cee2b355462931d7f7
Chheng Chanbo <chanbochheng4@gmail.com>
2026-06-16 21:19:10 +0700

    Fix Cambodia timezone

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -115,11 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
-LOGIN_URL = '/login/'
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Phnom_Penh'
 
 USE_I18N = True
 
